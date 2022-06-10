2cf4d2057531b36b9ce1eba316c6038b6cdea619
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-06-10 08:23:34 +0200

    remove files

--- tests/test_list_available_object_classifier_features.py
+++
@@ -1,4 +0,0 @@
-def test_list_available_object_classifier_features():
-    from apoc import list_available_object_classification_features
-
-    assert len(list_available_object_classification_features()) == 41
